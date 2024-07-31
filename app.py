5dfdc5660eaf7d17b0708561cf83118be2a508b9
Spartan1010 <tranulovianos@gmail.com>
2024-07-31 12:03:30 +0100

    Quiz Game Project v1

--- app.py
+++ app.py
@@ -1,23 +1,48 @@
-class User:
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
 
-    def __init__(self, user_id, first_name, last_name):         # Constructor
-        self.first_name = first_name      # Attributes
-        self.last_name = last_name
-        self.id = user_id
-        self.followers = 0
-        self.following = 0
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
 
 
-user_1 = User("001", "John", "Doe")
-user_2 = User("002", "Jane", "Doe")
 
-user_1.follow(user_2)
 
-print(user_1.followers)
-print(user_1.following)
-print(user_2.followers)
-print(user_2.following)
+
+
+
+# class User:
+#
+#     def __init__(self, user_id, first_name, last_name):         # Constructor
+#         self.first_name = first_name      # Attributes
+#         self.last_name = last_name
+#         self.id = user_id
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+#
+# user_1 = User("001", "John", "Doe")
+# user_2 = User("002", "Jane", "Doe")
+#
+# user_1.follow(user_2)
+#
+# print(user_1.followers)
+# print(user_1.following)
+# print(user_2.followers)
+# print(user_2.following)
